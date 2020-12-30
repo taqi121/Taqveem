167f9ed7663064f8cb79db33d5de253ac5f7f9bc
Taqveem Ahsan <taqi.malik86@gmail.com>
2020-12-30 18:05:25 +0500

    Add Changes till now

--- Point of sale system/Controllers/PurchaseController.cs
+++ Point of sale system/Controllers/PurchaseController.cs
@@ -3,15 +3,128 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Point_of_sale_system.Models;
+
 
 namespace Point_of_sale_system.Controllers
 {
     public class PurchaseController : Controller
     {
+        DbModelEntities dbModel = new DbModelEntities();
+        //try
+        //{
+        //    if (Session["UserName"] != null)
+        //    {
+
+        //    }
+        //    else
+        //    {
+        //        return RedirectToAction("Login", "MainControl");
+        //    }
+        //}
+        //catch
+        //{
+        //    return View();
+        //}
         // GET: Purchase
-        public ActionResult Index()
+        public ActionResult NewPurchase()
         {
+            //try
+            //{
+            //    if (Session["UserName"] != null)
+            //    {
+
+            //    }
+            //    else
+            //    {
+            //        return RedirectToAction("Login", "MainControl");
+            //    }
+            //}
+            //catch
+            //{
+            //    return View();
+            //}
             return View();
         }
+        [HttpPost]
+        public ActionResult newPurchase(purchase purchase)
+        {
+            //try
+            //{
+            //    if (Session["UserName"] != null)
+            //    {
+
+            //    }
+            //    else
+            //    {
+            //        return RedirectToAction("Login", "MainControl");
+            //    }
+            //}
+            //catch
+            //{
+            //    return View();
+            //}
+            return View();
+        }
+        public ActionResult PurchaseList()
+        {
+            try
+            {
+                if (Session["UserName"] != null)
+                {
+                    return View(dbModel.purchases.ToList());
+                }
+                else
+                {
+                    return RedirectToAction("Login", "MainControl");
+                }
+            }
+            catch
+            {
+                return View();
+            }
+
+        }
+        public ActionResult EditPurchase(int id)
+        {
+            try
+            {
+                if (Session["UserName"] != null)
+                {
+                    return View(dbModel.purchases.Where(x => x.ID == id).FirstOrDefault());
+                }
+                else
+                {
+                    return RedirectToAction("Login", "MainControl");
+                }
+            }
+            catch
+            {
+                return View();
+            }
+
+
+        }
+        [HttpPost]
+        public ActionResult EditPurchase(purchase purchase)
+        {
+            //try
+            //{
+            //    if (Session["UserName"] != null)
+            //    {
+
+            //    }
+            //    else
+            //    {
+            //        return RedirectToAction("Login", "MainControl");
+            //    }
+            //}
+            //catch
+            //{
+            //    return View();
+            //}
+            return View();
+        }
+
     }
 }
